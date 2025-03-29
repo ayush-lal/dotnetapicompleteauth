509806a17a5108a9796e0a6913842c52378bb46b
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-03-29 13:16:42 +0530

    feat: seeding admin record

--- DotnetApiCompleteAuth/Program.cs
+++ DotnetApiCompleteAuth/Program.cs
@@ -1,6 +1,21 @@
+using DotnetApiCompleteAuth.Data;
+using DotnetApiCompleteAuth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+string connectionString = builder.Configuration.GetConnectionString("default");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// for identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
 
 var app = builder.Build();
 
@@ -9,5 +24,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DbSeeder.SeedDataAsync(app);
 
 app.Run();
