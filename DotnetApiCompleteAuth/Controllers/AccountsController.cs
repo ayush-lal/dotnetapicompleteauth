56bed9ef833b71c6189838b9cd16b7c72c450cd2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-03-29 16:36:56 +0530

    feat: signup

---
+++ DotnetApiCompleteAuth/Controllers/AccountsController.cs
@@ -0,0 +1,89 @@
+using DotnetApiCompleteAuth.Constants;
+using DotnetApiCompleteAuth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetApiCompleteAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<AccountsController> _logger;
+
+        public AccountsController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ILogger<AccountsController> logger)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            try
+            {
+                var existingUser = await _userManager.FindByNameAsync(model.Email);
+                if (existingUser != null)
+                {
+                    return BadRequest("User already exists");
+                }
+
+                // Create User role if it doesn't exist
+                if ((await _roleManager.RoleExistsAsync(Roles.User)) == false)
+                {
+                    var roleResult = await _roleManager
+                          .CreateAsync(new IdentityRole(Roles.User));
+
+                    if (roleResult.Succeeded == false)
+                    {
+                        var roleErros = roleResult.Errors.Select(e => e.Description);
+                        _logger.LogError($"Failed to create user role. Errors : {string.Join(",", roleErros)}");
+                        return BadRequest($"Failed to create user role. Errors : {string.Join(",", roleErros)}");
+                    }
+                }
+
+                ApplicationUser user = new()
+                {
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = model.Email,
+                    Name = model.Name,
+                    EmailConfirmed = true
+                };
+
+                // Attempt to create a user
+                var createUserResult = await _userManager.CreateAsync(user, model.Password);
+
+                // Validate user creation. If user is not created, log the error and
+                // return the BadRequest along with the errors
+                if (createUserResult.Succeeded == false)
+                {
+                    var errors = createUserResult.Errors.Select(e => e.Description);
+                    _logger.LogError(
+                        $"Failed to create user. Errors: {string.Join(", ", errors)}"
+                    );
+                    return BadRequest($"Failed to create user. Errors: {string.Join(", ", errors)}");
+                }
+
+                // adding role to user
+                var addUserToRoleResult = await _userManager.AddToRoleAsync(user: user, role: Roles.User);
+
+                if (addUserToRoleResult.Succeeded == false)
+                {
+                    var errors = addUserToRoleResult.Errors.Select(e => e.Description);
+                    _logger.LogError($"Failed to add role to the user. Errors : {string.Join(",", errors)}");
+                }
+                return CreatedAtAction(nameof(Signup), null);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ DotnetApiCompleteAuth/Models/SignupModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DotnetApiCompleteAuth.Models;
+
+public class SignupModel
+{
+    [Required]
+    [MaxLength(30)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(30)]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(30)]
+    public string Password { get; set; } = string.Empty;
+
+}
